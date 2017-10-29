5ddebeac138cfadb3ffc9ffdca6d17d5befc767a
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-10-29 13:13:07 -0400

    done

--- work07.c
+++ work07.c
@@ -1,21 +1,27 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 int get_rand() {
   int random_file = 0;
   random_file = open("/dev/random", O_RDONLY);
   int rand = 0;
-  read(random_file, &rand, sizeof(rand));
-  close(random_file);
+  if (!read(random_file, &rand, sizeof(rand))) {
+    printf("%s", strerror(errno));
+  }
+  if (!close(random_file)) {
+    printf("%s", strerror(errno));
+  }
   return rand;
 }
 
 int main() {
   //need to check return values for errors
   
-  printf("Generating random numbers\n");
-  int * rands[11];
+  printf("Generating random numbers:\n");
+  int rands[11];
   int i = 0;
   for (i; i < 11; i ++) {
     rands[i] = get_rand();
@@ -24,16 +30,30 @@ int main() {
 
   printf("\nWriting numbers to file...\n\n");
   int write_file = 0;
-  write_file = open("numbers", O_CREAT | O_WRONLY);
-  write(write_file, &rands, sizeof(rands));
-  close(write_file);
+  write_file = open("numbers", O_CREAT | O_WRONLY, 0644);
+  if (!write_file) { 
+    printf("%s", strerror(errno));
+  }
+  if (!write(write_file, &rands, sizeof(rands))) {
+    printf("%s", strerror(errno));
+  }
+  if (!close(write_file)) {
+    printf("%s", strerror(errno));
+  }
 
   printf("Reading numbers from file...\n\n");
   int read_file = 0;
-  int * read_rands[11];
+  int read_rands[11];
   read_file = open("numbers", O_RDONLY);
-  read(read_file, &read_rands, sizeof(read_rands));
-  close(read_file);
+  if (!read_file) {
+    printf("%s", strerror(errno));
+  }
+  if (!read(read_file, &read_rands, sizeof(read_rands))) {
+    printf("%s", strerror(errno));
+  }
+  if (!close(read_file)) {
+    printf("%s", strerror(errno));
+  }
 
   printf("Verification that written values were the same:\n");
   i = 0;
